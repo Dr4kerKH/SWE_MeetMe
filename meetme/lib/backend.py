466a764576b78cfb3245146d352e55335899bf3e
Sonpaorong M. <muchhim.sonpaorong@gmail.com>
2025-03-06 19:31:26 -0600

    Oauth2 adds

--- meetme/lib/backend.py
+++ meetme/lib/backend.py
@@ -53,6 +53,16 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_email = payload.get("sub")
+        if user_email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_email
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
 ############################ API Endpoints ############################
 #######################################################################
 # Post Requests (Creating new records)
