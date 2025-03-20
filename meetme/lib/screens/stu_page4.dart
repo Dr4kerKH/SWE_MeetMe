a63e40402660fc67b4f4b9257d5f1e230696f11d
Sonpaorong M. <muchhim.sonpaorong@gmail.com>
2025-03-20 17:53:24 -0500

    Stud P2-4

--- meetme/lib/screens/stu_page4.dart
+++ meetme/lib/screens/stu_page4.dart
@@ -19,19 +19,56 @@ class _StudentPage4State extends State<StudentPage4> {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: [
-                  const Color.fromARGB(255, 255, 2, 2),
                   const Color.fromARGB(255, 255, 255, 255),
+                  const Color.fromARGB(255, 255, 199, 199),
                 ]
               ),
+              border: Border.all(
+                color: Colors.transparent,
+              ),
+              shape: BoxShape.rectangle,
             ),
           ),
-          Center(
-            child: const Text("Bonjour", 
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w600,
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(24),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(110, 0, 0, 0),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: TextField(
+                //controller: _userController,
+                textAlign: TextAlign.left,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 15,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w500,
+                ),
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search_rounded, color: Color.fromARGB(255, 0, 0, 0)),
+                  hintText: 'Search',
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(255, 145, 145, 145),
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  border: InputBorder.none,
+                ),
               ),
             ),
           ),
--- meetme/lib/student_pages.dart
+++ meetme/lib/student_pages.dart
@@ -5,7 +5,7 @@ import 'screens/stu_page1.dart';
 import 'screens/stu_page2.dart';
 import 'screens/stu_page3.dart';
 import 'screens/stu_page4.dart';
-import 'screens/stu_page5.dart';
+//import 'screens/stu_page5.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,6 +22,8 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+       centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         leading: IconButton(
           icon: const Icon(Icons.person_rounded, color: Color.fromARGB(255, 50, 50, 50)),
             onPressed: () {
@@ -37,8 +39,6 @@ class _HomePageState extends State<HomePage> {
             fontWeight: FontWeight.w600,
           ),
         ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout_rounded, color: Color.fromARGB(255, 50, 50, 50)),
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
               StudentPage2(),
               StudentPage3(),
               StudentPage4(),
-              StudentPage5(),
+              //StudentPage5(),
             ],
           ),
           Container(
