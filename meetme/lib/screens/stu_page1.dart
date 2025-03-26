b0865cf1354e0da9cb6ba4f5762806ddd2527e44
Sonpaorong M. <muchhim.sonpaorong@gmail.com>
2025-03-26 15:38:39 -0500

    StuPage1 & 2 update

--- meetme/lib/screens/stu_page1.dart
+++ meetme/lib/screens/stu_page1.dart
@@ -8,6 +8,98 @@ class StudentPage1 extends StatefulWidget {
 }
 
 class _StudentPage1State extends State<StudentPage1> {
+  
+  Future<void> _classAdder(BuildContext context) {
+    
+    final TextEditingController codeController = TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          title: Text(
+            'Class Registration',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).shadowColor,
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Please enter the code provided by your teacher',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  color: Theme.of(context).shadowColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: codeController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  hintText: 'Class Code Ex: ABCD1234',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                    color: Theme.of(context).hintColor,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  filled: true,
+                  fillColor: Theme.of(context).scaffoldBackgroundColor,
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Theme.of(context).secondaryHeaderColor,
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text(
+                'Register',
+                style: TextStyle(
+                  color: Theme.of(context).shadowColor,
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                ),
+              ),
+              onPressed: () {
+                final classCode = codeController.text;
+                if (classCode.isNotEmpty) {
+                  // Handle class registration logic here
+                  // Where it should search the class in DB and add it to classes' list
+                  // print('Class registered with code: $classCode');
+                }
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +111,78 @@ class _StudentPage1State extends State<StudentPage1> {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: [
-                  Theme.of(context).primaryColor,
+                  //Theme.of(context).primaryColor,
+                  Theme.of(context).secondaryHeaderColor,
                   Theme.of(context).scaffoldBackgroundColor,
                 ]
               ),
             ),
-            child: const Center(
-              child: Text(
-                'Student Page 1',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Classes', 
+                      style: TextStyle(
+                        fontSize: 24, 
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add, color: Theme.of(context).scaffoldBackgroundColor, size: 25,),
+                        onPressed: () => _classAdder(context),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Expanded(
+                  child: ListView.builder(
+                    padding: EdgeInsets.all(12),
+                    itemCount: 7,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(
+                            'CS-133${index + 1}-Computer Science ${index + 1}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).shadowColor,
+                            ),
+                          ),
+                            subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                            Text(
+                              'Rob LeGrand',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontStyle: FontStyle.italic,
+                                color: Theme.of(context).hintColor,
+                              ),
+                            ),
+                              SizedBox(height: 4),
+                              Text(
+                              '''This is a detailed description of the class.\n It will contain the more\n and more.''',
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Theme.of(context).hintColor,
+                              ),
+                              ),
+                            ],
+                            ),
+                          onTap: () {
+
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
